16f9282055ec09aae4759e470cf7d6eb06bf2e7a
Yuvraj Singh <qomfortzone@gmail.com>
2024-04-22 18:15:53 +0530

    Update

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapp
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // use arrayadapter and define an array
+        // Content Of list view.
         val arrayAdapter: ArrayAdapter<*>
         val users = arrayOf(
             "Virat Kohli", "Rohit Sharma", "Steve Smith",
@@ -24,5 +27,11 @@ class MainActivity : AppCompatActivity() {
         arrayAdapter = ArrayAdapter(this,
             android.R.layout.simple_list_item_1, users)
         mListView.adapter = arrayAdapter
+
+        mListView.setOnItemClickListener{ parent, view, position, id->
+            val text=(view as TextView).text.toString()
+            Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapp/User.kt
@@ -0,0 +1,8 @@
+package com.example.myapp
+
+data class User(
+    val name: Int,
+    val lastmsg: Int,
+    val image: Int,
+    val last_msg_time: Int
+)
